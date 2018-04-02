eb21c24985bb2cf70fd38135f59b776371ef2e17
Akwa Peter <akwapeter1@gmail.com>
2018-04-02 17:10:22 +0100

    splash and login ui

---
+++ app/src/main/java/com/example/rad5/med_manager/Login.java
@@ -0,0 +1,13 @@
+package com.example.rad5.med_manager;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Login extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
---
+++ app/src/main/java/com/example/rad5/med_manager/SplashScreen.java
@@ -0,0 +1,33 @@
+package com.example.rad5.med_manager;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SplashScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+
+        /*Create a thread method for the splash screen
+        * and override the run method
+        */
+        Thread my_thread = new Thread(){
+            @Override
+            public void run() {
+                try {
+                    Intent mIntent = new Intent(SplashScreen.this, MainActivity.class);
+                    sleep(3000);
+                    startActivity(mIntent);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        };
+
+        //Start the thread
+        my_thread.start();
+    }
+}
